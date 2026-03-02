e322b6c576ac537f51e7a262017147e8966e5cf8
Mitchell Shibilski-Unkel <mitchellshibs@gmail.com>
2026-03-01 20:04:29 -0700

    Core Func

--- Firefly/firefly.py
+++ Firefly/firefly.py
@@ -2,7 +2,7 @@ import os
 import json
 import flask
 import shutil
-import mediameta as mm
+import ffmpeg
 
 
 class Firefly:
@@ -32,8 +32,15 @@ class Firefly:
         self.directory = directory
         return self.info, self.directory
     
+    def secondsToTime(self, seconds):
+        minutes, secs = divmod(int(float(seconds)), 60)
+        hours, minutes = divmod(minutes, 60)
+        return f"{hours:02}:{minutes:02}:{secs:02}"
+    
     def getMovieInformation(self, dist: str):
-        return mm.VideoMetadata(os.path.join(os.getcwd(), dist))
+        info = ffmpeg.probe(os.path.join(os.getcwd(), "media/movies", dist))
+        info_dict = {"length": self.secondsToTime(info["format"]["duration"]), "year": info["format"]["tags"].get("creation_time", "Unknown")[:4], }
+        return info_dict
     
     def getMovies(self, dist: str = "movies.json"):
         with open(os.path.join(os.getcwd(), dist), "r") as f:         
@@ -49,20 +56,40 @@ class Firefly:
         getTitles = lambda x: x["title"]
         return list(map(getTitles, tv_shows))
     
-    def addMovie(self, filename: str, title: str = "Unknown"):
-        addFile = shutil.move(filename, os.path.join(os.getcwd(), "media/movies", filename))
-        addMovieToJSON = lambda x: {"title": title, "filename": filename}
-        with open(os.path.join(os.getcwd(), "movies.json"), "w") as f:
-            json.dump(addMovieToJSON(filename), f)
+    def loadMovie(self, title: str, dist: str = "movies.json"):
+        with open(os.path.join(os.getcwd(), dist), "r") as f:
+            movies = json.load(f)
+        for movie in movies:
+            if movie["title"] == title:
+                return movie
             
-        return "Added"
+        return None
     
-    def addTVShow(self, filename: str, title: str = "Unknown", season: int = 1, episode: int = 1):
-        addFile = shutil.move(filename, os.path.join(os.getcwd(), "media/tv", filename))
-        addTVShowToJSON = lambda x: {"title": title, "filename": filename, "season": season, "episode": episode}
-        with open(os.path.join(os.getcwd(), "tv.json"), "w") as f:
-            json.dump(addTVShowToJSON(filename), f)
+    def loadTVShow(self, title: str, dist: str = "tv.json"):
+        with open(os.path.join(os.getcwd(), dist), "r") as f:
+            tv_shows = json.load(f)
+        for show in tv_shows:
+            if show["title"] == title:
+                return show
             
+        return None
+    
+    def addMovie(self, filename: str, title: str = "Unknown"):
+        shutil.move(filename, os.path.join(os.getcwd(), "media/movies", filename))
+        with open(os.path.join(os.getcwd(), "movies.json"), "r") as f:
+            movies = json.load(f)
+        movies.append({"title": title, "filename": filename})
+        with open(os.path.join(os.getcwd(), "movies.json"), "w") as f:
+            json.dump(movies, f)
+        return "Added"
+
+    def addTVShow(self, filename: str, title: str = "Unknown", season: int = 1, episode: int = 1):
+        shutil.move(filename, os.path.join(os.getcwd(), "media/tv", filename))
+        with open(os.path.join(os.getcwd(), "tv.json"), "r") as f:
+            shows = json.load(f)
+        shows.append({"title": title, "filename": filename, "season": season, "episode": episode})
+        with open(os.path.join(os.getcwd(), "tv.json"), "w") as f:
+            json.dump(shows, f)
         return "Added"
     
     def run(self, host: str = "127.0.0.1", port: int = 5000):
@@ -70,11 +97,15 @@ class Firefly:
         
         @app.route("/", methods=["GET", "POST"])
         def index():
-            return flask.render_template(self.index)
+            movies = self.getMovies()
+            shows = self.getTVShows()
+            return flask.render_template(self.index, movies=movies, shows=shows)
         
         @app.route("/index", methods=["GET", "POST"])
         def home():
-            return flask.render_template(self.index)
+            movies = self.getMovies()
+            shows = self.getTVShows()
+            return flask.render_template(self.index, movies=movies, shows=shows)
         
         @app.route("/search", methods=["GET", "POST"])
         def search():
@@ -94,9 +125,29 @@ class Firefly:
         def addMedia():
             return flask.render_template("addMedia.html")
         
+        @app.route("/upload", methods=["POST"])
+        def upload():
+            file = flask.request.files.get("file")
+            mediaType = flask.request.form.get("type", "movie")
+            title = flask.request.form.get("title", "Unknown")
+            if not file or file.filename == "":
+                return "No file selected", 400
+            os.makedirs(os.path.join(os.getcwd(), "media/movies"), exist_ok=True)
+            os.makedirs(os.path.join(os.getcwd(), "media/tv"), exist_ok=True)
+            savePath = os.path.join(os.getcwd(), "media/movies" if mediaType == "movie" else "media/tv", file.filename)
+            file.save(savePath)
+            if mediaType == "movie":
+                self.addMovie(savePath, title)
+            else:
+                self.addTVShow(savePath, title)
+            return flask.redirect("/")
+        
         @app.route("/info/<movie>", methods=["GET", "POST"])
         def info(movie):
-            movie_info = self.getMovieInformation(movie)
-            return flask.render_template(self.info, title=movie_info.title, description=movie_info.description, genre=movie_info.genre, release_year=movie_info.release_year)
+            movie_info = self.loadMovie(movie)
+            if not movie_info:
+                return "Movie not found", 404
+            metadata = self.getMovieInformation(movie_info["filename"])
+            return flask.render_template(self.info, movie=movie_info["title"], time=metadata["length"], year=metadata["year"])
         
         app.run(host=host, port=port, debug=self.debug)
