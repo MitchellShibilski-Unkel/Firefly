7a162236e88c2c58489744a8e7ccc9770f0a401a
Mitchell Shibilski-Unkel <mitchellshibs@gmail.com>
2026-03-01 19:15:39 -0700

    Core Func

--- Firefly/firefly.py
+++ Firefly/firefly.py
@@ -1,5 +1,8 @@
 import os
+import json
 import flask
+import shutil
+import mediameta as mm
 
 
 class Firefly:
@@ -29,23 +32,71 @@ class Firefly:
         self.directory = directory
         return self.info, self.directory
     
+    def getMovieInformation(self, dist: str):
+        return mm.VideoMetadata(os.path.join(os.getcwd(), dist))
+    
+    def getMovies(self, dist: str = "movies.json"):
+        with open(os.path.join(os.getcwd(), dist), "r") as f:         
+            movies = json.load(f)
+        
+        getTitles = lambda x: x["title"]
+        return list(map(getTitles, movies))
+    
+    def getTVShows(self, dist: str = "tv.json"):
+        with open(os.path.join(os.getcwd(), dist), "r") as f:         
+            tv_shows = json.load(f)
+        
+        getTitles = lambda x: x["title"]
+        return list(map(getTitles, tv_shows))
+    
+    def addMovie(self, filename: str, title: str = "Unknown"):
+        addFile = shutil.move(filename, os.path.join(os.getcwd(), "media/movies", filename))
+        addMovieToJSON = lambda x: {"title": title, "filename": filename}
+        with open(os.path.join(os.getcwd(), "movies.json"), "w") as f:
+            json.dump(addMovieToJSON(filename), f)
+            
+        return "Added"
+    
+    def addTVShow(self, filename: str, title: str = "Unknown", season: int = 1, episode: int = 1):
+        addFile = shutil.move(filename, os.path.join(os.getcwd(), "media/tv", filename))
+        addTVShowToJSON = lambda x: {"title": title, "filename": filename, "season": season, "episode": episode}
+        with open(os.path.join(os.getcwd(), "tv.json"), "w") as f:
+            json.dump(addTVShowToJSON(filename), f)
+            
+        return "Added"
+    
     def run(self, host: str = "127.0.0.1", port: int = 5000):
-        app = flask.Flask(__name__)
+        app = flask.Flask(__name__, template_folder=os.path.join(os.getcwd(), self.directory), static_folder=os.path.join(os.getcwd(), "static"))
         
         @app.route("/", methods=["GET", "POST"])
+        def index():
+            return flask.render_template(self.index)
+        
+        @app.route("/index", methods=["GET", "POST"])
         def home():
-            return flask.send_from_directory(os.path.join(os.getcwd(), self.directory), self.index)
+            return flask.render_template(self.index)
         
-        @app.route("/movies", methods=["GET", "POST"])
-        def movies():
-            return flask.send_from_directory(os.path.join(os.getcwd(), self.directory), self.movies)
+        @app.route("/search", methods=["GET", "POST"])
+        def search():
+            search_term = flask.request.args.get("q", "")
+            movies = self.getMovies()
+            shows = self.getTVShows()
+            search_results = []
+            for movie in movies:
+                if search_term.lower() in movie.lower():
+                    search_results.append(movie)
+            for show in shows:
+                if search_term.lower() in show.lower():
+                    search_results.append(show)
+            return flask.render_template("search.html", search_results=search_results)
         
-        @app.route("/tv", methods=["GET", "POST"])
-        def tv():
-            return flask.send_from_directory(os.path.join(os.getcwd(), self.directory), self.tv)
+        @app.route("/addMedia", methods=["GET", "POST"])
+        def addMedia():
+            return flask.render_template("addMedia.html")
         
-        @app.route("/info", methods=["GET", "POST"])
-        def info():
-            return flask.send_from_directory(os.path.join(os.getcwd(), self.directory), self.info)
+        @app.route("/info/<movie>", methods=["GET", "POST"])
+        def info(movie):
+            movie_info = self.getMovieInformation(movie)
+            return flask.render_template(self.info, title=movie_info.title, description=movie_info.description, genre=movie_info.genre, release_year=movie_info.release_year)
         
         app.run(host=host, port=port, debug=self.debug)
--- test.py
+++ test.py
@@ -3,5 +3,6 @@ from Firefly.firefly import Firefly
 ff = Firefly(debug=True)
 
 ff.homeFile(filename="index.html", directory="templates")
+ff.infoPage(filename="info.html", directory="templates")
 
 ff.run()
